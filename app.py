4e590d3f0564dece73affae429a7924b1bc5d7f7
aguerrero2113 <aguerrero2113@github.com>
2023-01-29 13:52:06 -0600

    created a html file to display python data

--- app.py
+++ app.py
@@ -35,10 +35,67 @@ workout = random.choice(wod_type)
 
 
 
-triplet_wod2 = {
+triplet_wod_m = {
     workout:beg_wod1,
     meta:beg_met1,
     gym:beg_gym_move1,
     lift:beg_lift_move1
     }
-print((triplet_wod2))
+triplet_wod_g = {
+    workout:beg_wod1,
+    gym:beg_gym_move1,
+    meta:beg_met1,
+    lift:beg_lift_move1
+    }
+triplet_wod_l = {
+    workout:beg_wod1,
+    lift:beg_lift_move1,
+    meta:beg_met1,
+    gym:beg_gym_move1
+    }
+dup_wod_mg = {
+    workout:beg_wod1,
+    meta:beg_met1,
+    gym:beg_gym_move1
+    }
+dup_wod_ml = {
+    workout:beg_wod1,
+    meta:beg_met1,
+    lift:beg_lift_move1
+    }
+dup_wod_gm = {
+    workout:beg_wod1,
+    gym:beg_gym_move1,
+    meta:beg_met1
+    }
+dup_wod_gl = {
+    workout:beg_wod1,
+    gym:beg_gym_move1,
+    lift:beg_lift_move1
+    }
+dup_wod_lg = {
+    workout:beg_wod1,
+    lift:beg_lift_move1,
+    gym:beg_gym_move1
+    }
+dup_wod_lm = {
+    workout:beg_wod1,
+    lift:beg_lift_move1,
+    meta:beg_met1
+    }
+up_wod_m = {
+    workout:beg_wod1,
+    meta:beg_met1
+    }
+up_wod_g = {
+    workout:beg_wod1,
+    gym:beg_gym_move1
+    }
+up_wod_l = {
+    workout:beg_wod1,
+    lift:beg_lift_move1
+    }
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    return render_template('index.html',up_wod_l = up_wod_l)
