83487531837d669c4b64c620436ee153418a45ba
aguerrero2113 <aguerrero2113@github.com>
2023-02-10 17:49:55 -0600

    getting ready to add a login authorizatin feature to the webapp

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
 from flask import Flask, render_template,url_for,redirect
 from backend.functions import meta_week_1,gym_week_1,lift_week_1
-import requests
 
-import random
+
+
+
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRETKEY'
 
 
-
-
-# newString1 = str(up_wod_m1).replace("{",'').replace("}", '')
-# newString2 = str(dup_wod_mg).replace("{",'').replace("}", '')
-# newString3 = str(triplet_wod_m).replace("{",'').replace("}", '')
-# newString4 = str(dup_wod_ml).replace("{",'').replace("}", '')
-# newString5 = str(up_wod_m2).replace("{",'').replace("}", '')
-# newString6 = str(off).replace("{",'').replace("}", '')
-# newString7 = str(off).replace("{",'').replace("}", '')
 @app.route('/',methods=['GET','POST'])
 def index():
     return render_template('index.html')
+@app.route('/home',methods=['GET','POST'])
+def home():
+    return render_template('home.html')
 
 @app.route('/wodweek1',methods=['GET','POST'])
 def wod1():
@@ -32,3 +27,5 @@ def wod2():
 @app.route('/wodweek3',methods=['GET','POST'])
 def wod3():
     return render_template('wod_week_three.html',lift_week_1 = lift_week_1)
+
+
--- backend/functions.py
+++ backend/functions.py
@@ -1,6 +1,6 @@
 import random
 
-off = {'day':'Off Day'}
+off = ['Off Day']
 metabolic = ['Jump Rope', 'Burpees','Jumping Jacks','Mountain Climbers']
 gymnastics = ['air squat','pull up','push up', 'dip', 'sit up', 'lunge']
 weightlifting = ['deadlift','shoulder press','squat','kettlbell swing','thruster','benchpress']
@@ -58,117 +58,134 @@ workout5 = random.choice(wod_type)
 
 
 
+
 triplet_wod_m = {
     workout3:beg_wod3,
     meta3:beg_met3,
     gym3:beg_gym_move3,
     lift3:beg_lift_move3
     }
+
 triplet_wod_g = {
     workout3:beg_wod3,
     gym3:beg_gym_move3,
     meta3:beg_met3,
     lift3:beg_lift_move3
     }
+
 triplet_wod_l = {
     workout3:beg_wod3,
     lift3:beg_lift_move3,
     meta3:beg_met3,
     gym3:beg_gym_move3
     }
+
 dup_wod_mg = {
     workout2:beg_wod2,
     meta2:beg_met2,
     gym2:beg_gym_move2
     }
+
 dup_wod_ml = {
     workout1:beg_wod1,
     meta1:beg_met1,
     lift1:beg_lift_move1
     }
+
 dup_wod_gm = {
     workout2:beg_wod2,
     gym2:beg_gym_move2,
     meta2:beg_met2
     }
+
 dup_wod_gl = {
     workout1:beg_wod1,
     gym1:beg_gym_move1,
     lift1:beg_lift_move1
     }
+
 dup_wod_lg = {
     workout2:beg_wod2,
     lift2:beg_lift_move2,
     gym2:beg_gym_move2
 
     }
+
 dup_wod_lm = {
     workout4:beg_wod4,
     lift4:beg_lift_move4,
     meta4:beg_met4
     }
+
 up_wod_m1 ={
     workout4:beg_wod4,
     meta4:beg_met4
     }
+
 up_wod_g1 = {
     workout4:beg_wod4,
     gym4:beg_gym_move4
     }
+
 up_wod_l1 = {
     workout4:beg_wod4,
     lift4:beg_lift_move4
     }
+
 up_wod_m2 = {
     workout5:beg_wod5,
     meta5:beg_met5
     }
+
 up_wod_g2 = {
     workout5:beg_wod5,
     gym5:beg_gym_move5
     }
+
 up_wod_l2 = {
     workout5:beg_wod5,
     lift5:beg_lift_move5
     }
 
-# newString1 = str(up_wod_m1).replace("{",'').replace("}", '')
-# newString2 = str(dup_wod_mg).replace("{",'').replace("}", '')
-# newString3 = str(triplet_wod_m).replace("{",'').replace("}", '')
-# newString4 = str(dup_wod_ml).replace("{",'').replace("}", '')
-# newString5 = str(up_wod_m2).replace("{",'').replace("}", '')
-# newString6 = str(off).replace("{",'').replace("}", '')
-# newString7 = str(off).replace("{",'').replace("}", '')
+
 
 
 meta_week_1 = {
-    "day1":up_wod_m1,
-    "day2":dup_wod_ml,
-    "day3":triplet_wod_m,
-    "day4":dup_wod_mg,
-    "day5":up_wod_m2,
-    "day6":off,
-    "day7":off
+    "day1":str(up_wod_m1).replace(",","\n").replace("'","\n").replace("{","").replace("}", ""),
+    "day2":str(dup_wod_ml).replace(",","\n").replace("'","\n").replace("{","").replace("}", ""),
+    "day3":str(triplet_wod_m).replace(",","\n").replace("'","\n").replace("{","").replace("}", ""),
+    "day4":str(dup_wod_mg).replace(",","\n").replace("'","\n").replace("{","").replace("}", ""),
+    "day5":str(up_wod_m2).replace(",","\n").replace("'","\n").replace("{","").replace("}", ""),
+    "day6":str(off).replace(",","\n").replace("'","\n").replace("{","").replace("}", ""),
+    "day7":str(off).replace(",","\n").replace("'","\n").replace("{","").replace("}", "")
 }
 gym_week_1 = {
-    "day1":up_wod_g1,
-    "day2":dup_wod_gl,
-    "day3":triplet_wod_g,
-    "day4":dup_wod_gm,
-    "day5":up_wod_g2,
-    "day6":off,
-    "day7":off
-}
-lift_week_1 = {
-    "day1":up_wod_l1,
-    "day2":dup_wod_lm,
-    "day3":triplet_wod_l,
-    "day4":dup_wod_lg,
-    "day5":up_wod_l2,
-    "day6":off,
-    "day7":off
+    "day1":str(up_wod_g1).replace(",","\n").replace("'","\n").replace("{","").replace("}", ""),
+    "day2":str(dup_wod_gl).replace(",","\n").replace("'","\n").replace("{","").replace("}", ""),
+    "day3":str(triplet_wod_g).replace(",","\n").replace("'","\n").replace("{","").replace("}", ""),
+    "day4":str(dup_wod_gm).replace(",","\n").replace("'","\n").replace("{","").replace("}", ""),
+    "day5":str(up_wod_g2).replace(",","\n").replace("'","\n").replace("{","").replace("}", ""),
+    "day6":str(off).replace(",","\n").replace("'","\n").replace("{","").replace("}", ""),
+    "day7":str(off).replace(",","\n").replace("'","\n").replace("{","").replace("}", "")
 }
 
-print(meta_week_1)
-print(gym_week_1)
-print(lift_week_1)
+
+
+lift_week_1 = {
+    'day1':str(up_wod_l1).replace(",","\n").replace("'","\n").replace("{","").replace("}",""),
+    'day2':str(dup_wod_lm).replace(",","\n").replace("'","\n").replace("{","").replace("}",""),
+    'day3':str(triplet_wod_l).replace(",","\n").replace("'","\n").replace("{","").replace("}",""),
+    'day4':str(dup_wod_lg).replace(",","\n").replace("'","\n").replace("{","").replace("}",""),
+    'day5':str(up_wod_l2).replace(",","\n").replace("'","\n").replace("{","").replace("}",""),
+    'day6':str(off).replace(",","\n").replace("'","\n").replace("{","").replace("}",""),
+    'day7':str(off).replace(",","\n").replace("'","\n").replace("{","").replace("}","")
+}
+
+
+
+
+
+
+lift_week_one = str(lift_week_1).replace("{","").replace("}", "")
+gym_week_one = str(gym_week_1).replace("{","").replace("}", "")
+meta_week_one = str(meta_week_1).replace("{","").replace("}", "")
